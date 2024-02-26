7bb435493089a4090e4cf1a78116d7be1b06f10a
Thomas Burke <thomasburke9891@gmail.com>
2024-02-26 18:36:46 -0500

    Update app.js

--- arrays-01-starting-code/arrays-01-starting-code/app.js
+++ arrays-01-starting-code/arrays-01-starting-code/app.js
@@ -107,5 +107,37 @@ const taxAdjustedPrices = prices.map((price, index, prices) => {
   return priceObj;
 });
 
-console.log(prices, taxAdjustedPrices);
+// console.log(prices, taxAdjustedPrices);
 
+const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+console.log(sortedPrices);
+
+const filteredArray = prices.filter(price => price > 6);
+
+console.log(filteredArray);
+
+// let sum = 0;
+
+// prices.forEach((price) => {
+//   sum += price
+// })
+
+// console.log(sum);
+
+const sum = prices.reduce ((prevValue, curValue) => prevValue + curValue, 0);
+
+console.log(sum);
+
+const sum = prices.reduce ((prevValue, curValue) => {
+  return prevValue + curValue;
+}, 0);
+
+console.log(sum);
