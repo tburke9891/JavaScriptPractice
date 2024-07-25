10b9af2d021d778c3fa475245e3a61a485ffd90c
Thomas <thomasburke9891@gmail.com>
2024-07-25 15:25:50 -0400

    Removed unused content. Updated app.js

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Product {
-	title = "DEFAULT";
-	imageUrl;
-	description;
-	price;
+	//Fields
+	// title = "DEFAULT";
+	// imageUrl;
+	// description;
+	// price;
 
 	constructor(title, image, desc, price) {
 		this.title = title;
@@ -12,11 +13,60 @@ class Product {
 	}
 }
 
-console.log(new Product());
+class ShoppingCart {
+	items = [];
 
-const productList = {
-	// using class and constructor from above to replace the old code below (left for example)
-	products: [
+	addProduct(product) {
+		this.items.push(product);
+		this.totalOutput = `<h2>Total: \$${1}</h2>`;
+	}
+
+	render() {
+		const cartEl = document.createElement("section");
+		cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+		cartEl.className = "cart";
+		this.totalOutput = cartEl.querySelector("h2");
+		return cartEl;
+	}
+}
+
+// renders a product on the screen
+class ProductItem {
+	constructor(product) {
+		this.product = product;
+	}
+
+	addToCart() {
+		console.log("Adding product to cart...");
+		console.log(this.product);
+	}
+
+	render() {
+		const prodEl = document.createElement("li");
+		prodEl.className = "product-item";
+		prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt"${this.product.title}">
+        <div class="product-item__content">
+          <h2${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+		const addCartButton = prodEl.querySelector("button");
+		//bind the add to cart button to the product referenced with .bind(this)
+		addCartButton.addEventListener("click", this.addToCart.bind(this));
+		return prodEl;
+	}
+}
+
+class ProductList {
+	products = [
 		new Product(
 			"A Pillow",
 			"https://i.pinimg.com/736x/85/89/11/858911606f36337f38310e5478c8c9fb.jpg",
@@ -29,46 +79,35 @@ const productList = {
 			"A very plush carpet, the best carpet.",
 			386.01
 		),
-	],
-	// old code that required more typing to define different objects
-	// 	{
-	// 		title: "A Pillow",
-	// 		imageUrl:
-	// 			"https://i.pinimg.com/736x/85/89/11/858911606f36337f38310e5478c8c9fb.jpg",
-	// 		price: 19.99,
-	// 		description: "A very soft pillow.",
-	// 	},
-	// 	{
-	// 		title: "A Carpet",
-	// 		imageUrl:
-	// 			"https://i.pinimg.com/236x/ae/dc/62/aedc620031aa9f40b37c2bcf25d24135.jpg",
-	// 		price: 386.01,
-	// 		description: "A very plush carpet, the best carpet.",
-	// 	},
-	// ],
+	];
+
+	constructor() {}
 
 	render() {
-		const renderHook = document.getElementById("app");
 		const prodList = document.createElement("ul");
 		prodList.className = "product-list";
 		for (const prod of this.products) {
-			const prodEl = document.createElement("li");
-			prodEl.className = "product-item";
-			prodEl.innerHTML = `
-      <div>
-      <img src="${prod.imageUrl}" alt"${prod.title}">
-        <div class="product-item__content">
-          <h2${this.title}</h2>
-          <h3>\$${prod.price}</h3>
-          <p>${prod.description}</p>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      `;
+			const productItem = new ProductItem(prod);
+			const prodEl = productItem.render();
 			prodList.append(prodEl);
 		}
-		renderHook.append(prodList);
-	},
-};
+		return prodList;
+	}
+}
 
-productList.render();
+class Shop {
+	render() {
+		const renderHook = document.getElementById("app");
+
+		const cart = new ShoppingCart();
+		const cartEl = cart.render();
+		const productList = new ProductList();
+		const prodListEl = productList.render();
+
+		renderHook.append(cartEl);
+		renderHook.append(prodListEl);
+	}
+}
+
+const shop = new Shop();
+shop.render();
