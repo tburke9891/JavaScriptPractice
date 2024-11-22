eca38c60a1e611f2bcb76cd24f8547c114bbcbb1
Thomas <thomasburke9891@gmail.com>
2024-11-22 11:15:52 -0500

    concluded promises section in javascript udemy course

--- async-02-sync-code-loop/app.js
+++ async-02-sync-code-loop/app.js
@@ -23,26 +23,46 @@ const setTimer = (duration) => {
 	return promise;
 };
 
-function trackUserHandler() {
-	let positionData;
-	getPosition()
-		.then((posData) => {
-			positionData = posData;
-			return setTimer(2000); // first arguement is if promise passes, second would be failure( errors )
-		})
-		.catch((err) => {
-			console.log(err);
-		})
-		.then((data) => {
-			console.log(data, positionData);
-		});
-	setTimer(1000).then(() => {
-		console.log("Time done!");
-	});
-	console.log("Getting position...");
+async function trackUserHandler() {
+	// let positionData;
+	let posData;
+	let timerData;
+	try {
+		posData = await getPosition(); //waits for promise to pass/fail
+		timerData = await setTimer(2000);
+	} catch (error) {
+		console.log(error);
+	}
+	console.log(timerData, posData);
+	// .then((posData) => {
+	// 	positionData = posData;
+	// 	return setTimer(2000); // first arguement is if promise passes, second would be failure( errors )
+	// })
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	})
+	// 	.then((data) => {
+	// 		console.log(data, positionData);
+	// 	});
+	// setTimer(1000).then(() => {
+	// 	console.log("Time done!");
+	// });
+	// console.log("Getting position...");
 }
 
 button.addEventListener("click", trackUserHandler);
+
+// Promise.race([getPosition(), setTimer(1000)]).then((data) => {
+// 	console.log(data);
+// });
+
+// Promise.all([getPosition(), setTimer(1000)]).then((promiseData) => {
+// 	console.log(promiseData);
+// });
+
+Promise.allSettled([getPosition(), setTimer(1000)]).then((promiseData) => {
+	console.log(promiseData);
+});
 
 // let result = 0;
 // for (let i = 0; i < 100000000; i++) {
